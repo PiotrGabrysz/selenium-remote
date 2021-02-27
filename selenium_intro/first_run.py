ff6261f2ece72b610adef77572a7d95bd800c041
BRL021\brl021.krk <dawid.pacia@brainly.com>
2021-02-27 12:12:26 +0100

    selectors

--- selenium_intro/first_run.py
+++ selenium_intro/first_run.py
@@ -10,6 +10,18 @@ driver.get("http://automationpractice.com")
 
 driver.find_element_by_class_name("shop-phone")
 
+# common finders
+driver.find_element_by_id("header_logo")
+driver.find_element_by_partial_link_text("Cart")
+driver.find_element_by_id("newsletter-input")
+driver.find_element_by_class_name("twitter")
+driver.find_element_by_class_name("homefeatured")
+driver.find_element_by_id("contact-link")
+
+# css selectors
+driver.find_element_by_css_selector("")
+
+
 # elements = driver.find_elements_by_class_name("product-container-test")
 # assert elements, "Elements not found"
 
